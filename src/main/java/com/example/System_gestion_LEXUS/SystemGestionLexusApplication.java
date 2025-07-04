798483d424b876ea04d797a880cffb349111031c
NightGiven <marouane.marhoussi@gmail.com>
2025-07-04 02:19:43 +0100

    first commit

---
+++ src/main/java/com/example/System_gestion_LEXUS/SystemGestionLexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.System_gestion_LEXUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemGestionLexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemGestionLexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/System_gestion_LEXUS/SystemGestionLexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.System_gestion_LEXUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemGestionLexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
